284c1b364d02466d62a76f6cbee4d19dfebb75e2
gusflopes <gusflopes86@gmail.com>
2019-11-07 09:43:13 -0400

    feat: SigIn Authentication

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.png';
 
@@ -13,8 +16,11 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
   function handleSubmit({ email, password }) {
-    console.log(`Email: ${email} e Password: ${password}`);
+    dispatch(signInRequest(email, password));
   }
 
   return (
@@ -27,7 +33,9 @@ export default function SignIn() {
         <strong>SUA SENHA</strong>
         <Input name="password" type="password" placeholder="*********" />
 
-        <button type="submit">Entrar no Sistema</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no Sistema'}
+        </button>
       </Form>
     </>
   );
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,29 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import { signInSuccess, signFailure } from './actions';
+
+import history from '~/services/history';
+import api from '../../../services/api';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/students');
+  } catch (err) {
+    toast.error('Falha na autenticação.');
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
