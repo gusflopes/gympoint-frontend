29b231d0192fbc3b04275fb5f981c152c2fb0f5f
gusflopes <gusflopes86@gmail.com>
2019-11-07 13:19:16 -0400

    feat: Send JWT on every request

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Students() {
+  api.get('students');
   return <h1>Students</h1>;
 }
